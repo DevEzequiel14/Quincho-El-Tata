921956761aee8eb6d173ae053d12ad8be84963d3
Ezequiel Chorolque <ezequielchorolque14@gmail.com>
2025-02-17 18:14:00 -0300

    add the startup masthead

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../shared/components/header/header.component";
+import { LinkComponent } from "../../shared/components/link/link.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, LinkComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
+  buttonText: string = 'Mas información';
+  ref: string = '#info'
 }
