fef572b2214325b116a6ee8578512328960e3679
Ezequiel Chorolque <ezequielchorolque14@gmail.com>
2025-02-20 14:56:31 -0300

    add the contact and social networks section

---
+++ src/app/pages/home/components/contact/contact.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SocialComponent } from '../social/social.component';
+
+@Component({
+  selector: 'app-contact',
+  imports: [SocialComponent],
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.scss'
+})
+export class ContactComponent {
+
+}
