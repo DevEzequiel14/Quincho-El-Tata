7b49965e69749760ba4fe7ccf5a88bd84a6b3f9f
Ezequiel Chorolque <ezequielchorolque14@gmail.com>
2025-02-21 11:13:45 -0300

    refactor home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../shared/components/header/header.component";
-import { LinkComponent } from "../../shared/components/link/link.component";
-import { GoogleMapComponent } from "../../shared/components/google-map/google-map.component";
+import { FooterComponent } from '../../shared/components/footer/footer.component';
+import { AboutComponent } from "./components/about/about.component";
+import { ContactComponent } from './components/contact/contact.component';
+import { BenefitsComponent } from "./components/benefits/benefits.component";
+import { LocationComponent } from "./components/location/location.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, LinkComponent, GoogleMapComponent],
+  imports: [HeaderComponent, FooterComponent, AboutComponent, ContactComponent, BenefitsComponent, LocationComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
-  buttonText: string = 'Mas información';
-  ref: string = '#info'
 }
