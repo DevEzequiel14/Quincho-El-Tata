3778e9f370a2c9d4aefdeb2bba528fabcbde80bc
Ezequiel Chorolque <ezequielchorolque14@gmail.com>
2025-03-21 16:42:57 -0300

    change the location of the whatsapp component and add animations to different components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { WhatsappComponent } from './shared/components/whatsapp/whatsapp.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, WhatsappComponent],
+  imports: [RouterOutlet, ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,10 +5,21 @@ import { AboutComponent } from "./components/about/about.component";
 import { ContactComponent } from './components/contact/contact.component';
 import { BenefitsComponent } from "./components/benefits/benefits.component";
 import { LocationComponent } from "./components/location/location.component";
+import { ScrollAnimateDirective } from '../../shared/directives/scroll-animate.directive';
+import { WhatsappComponent } from '../../shared/components/whatsapp/whatsapp.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, FooterComponent, AboutComponent, ContactComponent, BenefitsComponent, LocationComponent],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    ContactComponent,
+    BenefitsComponent,
+    LocationComponent,
+    ScrollAnimateDirective,
+    WhatsappComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
