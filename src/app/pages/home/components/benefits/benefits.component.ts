3a128c7bb3922aaf085f7f56c176d71cd78699a4
Ezequiel Chorolque <ezequielchorolque14@gmail.com>
2025-03-21 18:15:57 -0300

    change to static texts

--- src/app/pages/home/components/benefits/benefits.component.ts
+++ src/app/pages/home/components/benefits/benefits.component.ts
@@ -8,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class BenefitsComponent {
 
+  mainTitle = 'Servicio principal'
+  mainDescription = 'Quincho con pileta y espacio verde para poder disfrutar un lindo día junto a tus seres queridos.'
+  main = [
+    'Asadores techados, cocina con horno, heladera, freezer y vajilla de vidrio.',
+    'Pileta de 8x4m + pileta pequeña de 2x2m.',
+    'Mesas, sillas, bancas',
+    'Manteles',
+    'Baños completos',
+    'Wifi',
+  ]
+
+  additionalTitle = 'Servicios adicionales'
+  additional = [
+    'Catering: Disfrutá de tu evento con tranquilidad, nosotros nos encargamos del resto.',
+    'Contratos, presupuestos y asesoramientos personalizados.',
+  ]
+
+  optionalTitle = 'Servicios opcionales'
+  optional = [
+    'Parrilleros',
+    'Barra móvil: tragos y bebidas con y sin alcohol.',
+    'Bartender',
+  ]
 }
